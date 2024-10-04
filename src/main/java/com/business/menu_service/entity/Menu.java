db7037d41326e44adc5a7017df62ac27eb8c20f4
TrucNguyen2102 <nguyenthanhtruc02012002@gmail.com>
2024-10-04 17:29:20 +0700

    update entity

---
+++ src/main/java/com/business/menu_service/entity/Menu.java
@@ -0,0 +1,48 @@
+package com.business.menu_service.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "menu")
+public class Menu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "item_name", length = 50, nullable = false)
+    private String itemName;
+
+    @Column(name = "price")
+    private Double price;
+
+    public Menu(Integer id, String itemName, Double price) {
+        this.id = id;
+        this.itemName = itemName;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
